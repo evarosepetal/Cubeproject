d25e99b07731e32544693d98e6052219182208c5
evarosepetal <evarosepetal99@gmai.com>
2022-10-15 14:18:03 +0100

    Signed-off-by: evarosepetal <evarosepetal99@gmai.com>

---
+++ The cube game/Assets/Buslights.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Buslights : MonoBehaviour 
+{ Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            light.enabled = !light.enabled;
+        }
+    }
+}
